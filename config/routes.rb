ecc9d367508e4c615eedc8de3a5e2f082ccd5a06
Ehsanur Rahman <esnr.rahman@gmail.com>
2017-07-23 18:32:12 -0400

    Finish patient and encounter details page
    - Fix routing: Have nested routes now
    - Make sure all paths work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :encounters
-  resources :patients
-
+  resources :patients do
+    resources :encounters
+  end
   root "patients#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
